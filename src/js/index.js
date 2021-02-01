357cca9d86ea7f26ac56c66313259af9edb21bce
Bekzod <bek.bek.bek.bek72@gmail.com>
2021-02-01 19:50:48 +0500

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import {
+    onMouseClick,
+    fixHeader,
+    hideMouseIcon
+} from "./js/scroll.js";
+import "./css/style.scss";
+
+
+onMouseClick();
+fixHeader();
+hideMouseIcon();
+
+
+console.log("it works")
